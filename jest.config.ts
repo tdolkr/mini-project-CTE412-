6c4adacafec5d74c012a60e87a6d01ca9094f694
tdolkr <tashidolkss@gmail.com>
2025-10-20 17:08:07 +0600

    Fix TS build config for CI

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,11 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.jest.json'
+    }
+  },
   roots: ['<rootDir>/tests'],
   setupFiles: ['<rootDir>/tests/setupEnv.ts'],
   moduleNameMapper: {
